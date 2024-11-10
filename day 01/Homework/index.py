2f743ed41cdca94a4a05d89dd9dba0e5a1a9c18a
nukigagnidze <marisefashvili>
2024-11-09 22:45:15 -0800

    GOA BEST

--- day 01/Homework/index.py
+++ day 01/Homework/index.py
@@ -9,3 +9,16 @@ if sum % 2 == 0:
     print("მარტივია")
 else:
     print("არამარტივია")
+
+
+# 2
+num = int(input("Enter a number: "))
+sum = 0
+
+for i in str(num):
+    sum += int(i)
+
+if sum % 5 == 0:
+    print("მარტივია")
+else:
+    print("არამარტივია")
